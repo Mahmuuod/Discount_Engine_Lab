3628da468d19efccc4e0568a648b2b3be37b3bfb
Mahmoud <osamamahmod657@gmail.com>
2025-05-22 13:07:29 +0300

    initial commit

---
+++ src/main/scala/main.scala
@@ -0,0 +1,22 @@
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Date
+import scala.io.Source
+import java.time.Instant
+import java.time.{MonthDay ,ZoneId}
+import java.sql.{Connection, DriverManager, ResultSet,PreparedStatement}
+import java.io.{FileWriter, PrintWriter}
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+object main {
+  def main(args: Array[String]): Unit = {
+
+
+
+
+
+
+
+  }
+}
+
