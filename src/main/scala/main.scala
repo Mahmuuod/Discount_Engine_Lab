40e1e8953224b663eab0dca70f836540b9a70361
Mahmoud <osamamahmod657@gmail.com>
2025-05-22 13:13:43 +0300

    Reading Data and Adding Some Calculated Fields

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -12,6 +12,31 @@ object main {
   def main(args: Array[String]): Unit = {
 
 
+    //timestamp,product_name,expiry_date,quantity,unit_price,channel,payment_method
+    //timestamp,product_name,expiry_date,quantity,unit_price,daysBetween
+    val lines = Source.fromFile("D:\\study\\iti\\Scala\\scala-iti45\\Project\\TRX1000.csv").getLines().toList.tail
+
+    //gets the data in suitable format with some calculations
+    val orders = (lines).map(x => {
+      val y = x.split(",")
+      //converting to time stamp
+      val instant = Instant.parse(y(0))
+      val timestamp = Timestamp.from(instant)
+
+      val product_name = y(1)
+      val dateFormat = new SimpleDateFormat("yyyy-MM-dd") // adjust format as per your date string
+      val expiry_date: Date = dateFormat.parse(y(2))
+      val quantity = y(3).toInt
+      val unit_price = y(4).toDouble
+      val transaction_date: Date = new Date(timestamp.getTime)
+      val daysBetween = (expiry_date.getTime - transaction_date.getTime) / (1000 * 60 * 60 * 24) //get time returns milli secs
+      val wine_cheese = y(1).split(" ")(0)
+      val channel = y(5)
+      val payment_method = y(6)
+      val total = quantity.toDouble * unit_price
+      (timestamp, product_name, expiry_date, quantity, unit_price, daysBetween.toInt, wine_cheese, total, channel, payment_method)
+    })
+
 
 
 
